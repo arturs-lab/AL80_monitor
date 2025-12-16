85874933e3c833290265d8f5a01c688b5cf22f03
Artur's Lab <mac_it_man@yahoo.com>
2025-12-16 00:21:15 -0600

    add ISR for ctc and SIO RX

--- irqtab.asm
+++ irqtab.asm
@@ -6,18 +6,18 @@ IRQTABR:		; irq tab for IM2 and for NMI, IRQ
 
 SIOB_TX_BUF_E:	DEFW irq_x_1	; 00 SIO B TX buffer empty
 SIOB_CHG_STAT:	DEFW irq_x_1	; 01 SIO B change/status
-SIOB_RX_CH_CON:	DEFW irq_x_1	; 02 SIO B RX char condition available
+SIOB_RX_CH_CON:	DEFW SIOB_ISR	; 02 SIO B RX char condition available
 SIOB_RX_SPEC:	DEFW irq_x_1	; 03 SIO B RX special condition
 
 SIOA_TX_BUF_E:	DEFW irq_x_1	; 04 SIO A TX buffer empty
 SIOA_CHG_STAT:	DEFW irq_x_1	; 05 SIO A TX change/status
-SIOA_RX_CH_CON:	DEFW irq_x_1	; 06 SIO A RX char condition 
+SIOA_RX_CH_CON:	DEFW SIOA_ISR	; 06 SIO A RX char condition 
 SIOA_RX_SPEC:	DEFW irq_x_1	; 07 SIO A RX special condition
 
 CTC_CH0_INT:	DEFW CTC_T0_ISR	; 08 
 CTC_CH1_INT:	DEFW CTC_T1_ISR	; 09 
-CTC_CH2_INT:	DEFW irq_x_1	; 0A 
-CTC_CH3_INT:	DEFW irq_x_1	; 0B 
+CTC_CH2_INT:	DEFW CTC_T2_ISR	; 0A 
+CTC_CH3_INT:	DEFW CTC_T3_ISR	; 0B 
 
 PIOA_INTV:		DEFW irq_x_1	; 0C 
 PIOB_INTV:		DEFW irq_x_1	; 0D 
